07d1b22a3e79b8a89edd627397e44113084eff56
bbrshkh <babarsheikh6794@gmail.com>
2024-04-09 16:51:15 +0530

    checking for error

--- db.js
+++ db.js
@@ -8,7 +8,7 @@ const mongoURL = process.env.MONGODB_URL;
 
 mongoose.connect(mongoURL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    // useUnifiedTopology: true
 });
 
 const db = mongoose.connection;
