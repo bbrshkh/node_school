d4ab0d94a7002492fd6d2cac47a9f8a2826cea51
bbrshkh <babarsheikh6794@gmail.com>
2024-04-09 15:23:46 +0530

    env final

--- db.js
+++ db.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 require('dotenv').config()
 
-// const mongoURL = 'mongodb://localhost:27017/school';
-// const mongoURL = 'mongodb+srv://school:school123@cluster0.mr6vz9n.mongodb.net/'
+// const mongoURL = process.env.MONGODB_LOCAL_URL;
 const mongoURL = process.env.MONGODB_URL;
 
 
